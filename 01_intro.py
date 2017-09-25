2342710af76941e2b268d31c0d62d9bbfb9815ed
BrancoBaertNMCT <branco.baert@student.howest.be>
2017-09-25 22:27:37 +0200

    Initial commit

---
+++ 01_intro.py
@@ -0,0 +1,45 @@
+# Labo 01 - basis variabelen
+
+#print("Hello World ,  \n  hello Branco ")
+#print(1+1)
+#leeftijd =  input("What is your age? ") #is een string
+
+
+# #oefening 1
+# base = input("Base of triangle ")
+# height = input("Height of triangle ")
+# #datatype conversie int float ...
+# surface = (int(base)*int(height))/2
+# print("The surface of the triangle is {0: .2f}".format(surface))
+
+# #oefening 2
+# days = input("Quantity of days ")
+# hours = input("Quantity of hours ")
+# minutes = input("Quantity of minutes ")
+# seconds = input("Quantity of seconds ")
+# seconds = (int(seconds)+(int(minutes)*60)+(int(hours)*3600)+(int(days)*86400))
+# print ("The total of seconds is {0}".format(seconds))
+
+# #oefening 3
+# second = input("Quantity of seconds ")
+# d = (int((int(second)/86400)))
+# h = int(int((int(second)-((int(d)*86400))))/3600)
+# m = int(int((int(hr)-(3600*int(h))))/60)
+# s = int(int(mr)-(int(m)*60))
+# print("D:H:M:S -> {0}:{1}:{2}:{3}".format(d,h,m,s))
+
+# #oefening 4
+# n = input("What value has N? ")
+# nn = n+n
+# nnn = n+n+n
+# result = (int(n)+int(nn)+int(nnn))
+# print ("Total: {0}".format(result))
+
+# #oefening 5
+# x = input("What value has X? ")
+# y = input("What value has Y? ")
+# result = (int(x)+int(y))*(int(x)+int(y))
+# print ("({0}+{1})^2 = {2}".format(x,y,result))
+
+# import sys
+# print(dir(sys))
